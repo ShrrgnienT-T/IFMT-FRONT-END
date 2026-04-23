55851e3047d2b278b7dbf019b110aed0b33f4a65
Shrrgnien <eduardo.limaoliveira.50@gmail.com>
2026-04-22 20:19:36 -0400

    adiciona arquivos HTML e JavaScript para exercícios de prática

---
+++ EXERCICIO1.js
@@ -0,0 +1,7 @@
+let nome = "eduardo";
+let idade = 20;
+let valorLogico = true;
+
+console.log("O nome é: " + nome + " | Tipo: " + typeof nome);
+console.log("A idade é: " + idade + " | Tipo: " + typeof idade);
+console.log("O valor lógico é: " + valorLogico + " | Tipo: " + typeof valorLogico);
